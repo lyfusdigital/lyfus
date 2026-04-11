45744e580bc094cfea693544c003d3abd903a34e
lyfusdigital <g.newburgh@gmail.com>
2026-04-11 12:30:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const lyfus = document.getElementById("lyfus");
+const sounddesign = document.getElementById("sounddesign");
 
 const fonts = [
     "serif",
@@ -19,6 +20,8 @@ const fonts = [
 function randomFont() {
     const pick = fonts[Math.floor(Math.random() * fonts.length)];
     lyfus.style.fontFamily = pick;
+    sounddesign.style.fontFamily = pick;
 }
 
 window.addEventListener("wheel", randomFont);
+window.addEventListener("touchmove", randomFont);
